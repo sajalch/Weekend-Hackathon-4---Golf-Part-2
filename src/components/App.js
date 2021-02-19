c2d965217880be47b193652c5e91c5c60209bfbb
sajalc <sajalchakraborty7@gmail.com>
2021-02-19 19:29:19 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { Component, useState } from "react";
+import React, { useEffect, useState } from "react";
 import "../styles/App.css";
 
 const App = () => {
@@ -9,9 +9,57 @@ const App = () => {
     left: "0px",
     top: "0px",
   });
-  const reset = () => {};
-  const renderChoice = () => {};
+  useEffect(()=>{
+    setBallPosition({
+    left: x+"px",
+    top: y+"px",
+  })},[x,y]);
 
+  const reset = () => {
+    setRenderBall(false);
+    setX(0);
+    setY(0);
+  };
+
+  const renderChoice=()=>{
+    if (renderBall) {
+      return <div className="ball"
+           style={
+               {
+                   left :x+"px",
+                   top:y+"px",
+
+              }}></div>
+  } else {
+      return <button className="start" onClick={()=>setRenderBall(true)} >Start</button>
+  }
+  }
+  const keyDLogic = (event)=>{
+    switch(event.keyCode){
+        case 39:{  //right
+            setX(x+5);
+            break;
+        }
+        case 40:{ //up
+          setY(y+5);
+            break;
+        }
+        case 37:{   //left
+          setX(x-5);
+            break;
+        }
+        case 38:{   //down
+          setY(y-5);
+            break;
+        }
+        default: break;
+    }
+}
+useEffect(()=>{
+  document.addEventListener("keydown",keyDLogic);
+  return ()=>document.removeEventListener("keydown",keyDLogic);
+}
+);
   return (
     <div className="playground">
       <button onClick={reset} className="reset">
